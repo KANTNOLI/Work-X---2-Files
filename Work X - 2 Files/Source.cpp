8ca9c33d16cb7ded148ffc226c4052213dfd6dde
KANTNOLI <zahargysev02@gmail.com>
2023-06-14 22:13:38 +0300

    ready

---
+++ Work X - 2 Files/Source.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+int main(void) {
+	setlocale(LC_ALL, "RUS");
+	string str1, str2;
+	string file1, file2;
+	string msg = "";
+
+	cout << "¬ведите название 1-го файла: ";
+	cin >> file1;
+	file1 += ".txt";
+
+	cout << "¬ведите название 2-го файла: ";
+	cin >> file2; 
+	file2 += ".txt";
+
+	ifstream fin1;  
+	ifstream fin2;
+
+	fin1.open(file1);
+	fin2.open(file2);
+
+	if (!fin1.is_open() or !fin2.is_open()) {
+		cout << "ERROR 1" << endl;
+	}
+	else {
+		while (!fin1.eof()) {
+			getline(fin1, str1); 
+			getline(fin2, str2);
+
+			if (str1 != str2) {
+				msg += "‘айлы не одинаковые!!!";
+				break;
+			}
+			msg += "‘айлы одинаковые";
+		}
+
+		fin1.close(); 
+		fin2.close();
+	}
+
+	cout << msg << endl;
+
+	return 0;
+}
